b40a6032dfd7351380e1cc5934de502562762b0a
dan-valkyrie <dan@valkyrie-vr.com>
2024-10-24 14:22:59 +0100

    Update CalibrationButton.cs
    Improved the running of this script by updating values on OnEnable.

--- Samples~/EIRExamples/EIR Configuration/CalibrationButton.cs
+++ Samples~/EIRExamples/EIR Configuration/CalibrationButton.cs
@@ -23,16 +23,20 @@ namespace Valkyrie.EIR.Examples {
         #region Private Variables
 
         private int handIndex;
+        private TextMeshProUGUI text;
 
         #endregion
 
         #region Unity Methods
 
-        private void Start() {
+        private void OnEnable() {
             handIndex = isLeft ? 1 : 0;
 #if EIR_HAPTICS
-            if (label != null)
-                label.GetComponent<TextMeshProUGUI>().text = (EIRManager.Instance.Haptics.CalibrationIndex[handIndex] + 1).ToString();
+            if (label != null && text == null) text = label.GetComponent<TextMeshProUGUI>();
+            if (text != null) {
+                Debug.Log($"[Calibration Button] Initialising with value {EIRManager.Instance.Haptics.CalibrationIndex[handIndex]}"); 
+                text.SetText((EIRManager.Instance.Haptics.CalibrationIndex[handIndex] + 1).ToString());
+            }
 #endif
         }
 
@@ -51,8 +55,7 @@ namespace Valkyrie.EIR.Examples {
             if (up) {
                 if (currentIndex < HapticManager.CALIBRATION_INDEX_LENGTH)
                     currentIndex += 1;
-            }
-            else {
+            } else {
                 if (currentIndex > 0)
                     currentIndex -= 1;
             }
@@ -65,7 +68,7 @@ namespace Valkyrie.EIR.Examples {
 
             EIRManager.Instance.Haptics.ModifyCalibrationByIndex(isLeft, currentIndex);
             label.GetComponent<TextMeshProUGUI>().text = (currentIndex + 1).ToString(); // to count from 1 to 11, instead of 0 to 10
-			#endif
+#endif
         }
 
         #endregion
